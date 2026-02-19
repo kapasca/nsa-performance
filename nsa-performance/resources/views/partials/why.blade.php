a1226ab15aa0ccf5c1d2a2b5da608dc89a61c358
Kapasca <kapasca@techgrid.co>
2026-02-19 14:40:18 +0700

    Add image to why-section

--- nsa-performance/resources/views/partials/why.blade.php
+++ nsa-performance/resources/views/partials/why.blade.php
@@ -4,20 +4,28 @@
 
     <div class="row">
       <div class="col-md-4">
-        <img height="120" src="{{ asset('assets/images/logo/part-quality.svg') }}" alt="High Quality Parts" class="mb-3">
+        <img height="150" src="{{ asset('assets/images/logo/part-quality.svg') }}" alt="High Quality Parts" class="why-image mb-3">
         <h5>High Quality Parts</h5>
         <p>Premium racing-grade components.</p>
       </div>
       <div class="col-md-4">
-        <img height="120" src="{{ asset('assets/images/logo/value-for-money.svg') }}" alt="High Quality Parts" class="mb-3">
+        <img height="150" src="{{ asset('assets/images/logo/value-for-money.svg') }}" alt="High Quality Parts" class="why-image mb-3">
         <h5>Affordable Pricing</h5>
         <p>Competitive prices for enthusiasts.</p>
       </div>
       <div class="col-md-4">
-        <img height="120" src="{{ asset('assets/images/logo/party-fun.svg') }}" alt="High Quality Parts" class="mb-3">
+        <img height="150" src="{{ asset('assets/images/logo/party-fun.svg') }}" alt="High Quality Parts" class="why-image mb-3">
         <h5>Trusted by Riders</h5>
         <p>Thousands of satisfied customers.</p>
       </div>
     </div>
   </div>
 </section>
+
+@push('styles')
+<style>
+  .why-image {
+    filter: invert(50%);
+  }
+</style>
+@endpush
